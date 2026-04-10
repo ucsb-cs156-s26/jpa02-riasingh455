496a3864ac1499c78782156b85d0040dcd9fbf52
riasingh455 <riasingh@ucsb.edu>
2026-04-09 17:35:48 -0700

    tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,7 +24,46 @@ public class TeamTest {
         assertEquals("Team(name=test-team, members=[])", team.toString());
     }
     @Test
-    void hashCode_whenObjectsEqual_returnsSameValue() {
+    void equals_returns_false_when_names_differ() {
+        Team t1 = new Team();
+        t1.setName("foo");
+
+        Team t2 = new Team();
+        t2.setName("bar");
+
+        assertFalse(t1.equals(t2));
+    }
+
+    @Test
+    void equals_returns_false_when_members_differ() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("baz");
+
+        assertFalse(t1.equals(t2));
+    }
+
+    @Test
+    void equals_returns_true_when_same_object() {
+        assertTrue(team.equals(team));
+    }
+
+    @Test
+    void equals_returns_false_when_object_is_null() {
+        assertFalse(team.equals(null));
+    }
+
+    @Test
+    void equals_returns_false_when_object_is_different_type() {
+        assertFalse(team.equals("not a team"));
+    }
+
+    @Test
+    void equals_returns_true_when_names_and_members_are_same() {
         Team t1 = new Team();
         t1.setName("foo");
         t1.addMember("bar");
@@ -32,17 +72,27 @@ public class TeamTest {
         t2.setName("foo");
         t2.addMember("bar");
 
-        assertEquals(t1, t2);
+        assertTrue(t1.equals(t2));
+    }
+
+    @Test
+    void hashCode_returns_same_value_when_teams_are_equal() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+
         assertEquals(t1.hashCode(), t2.hashCode());
     }
+
     @Test
     void hashCode_returns_expected_value_for_empty_team() {
         Team t = new Team();
-        int result = t.hashCode();
-        int expectedResult = 1;
-        assertEquals(expectedResult, result);
+        assertEquals(1, t.hashCode());
     }
-   
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
